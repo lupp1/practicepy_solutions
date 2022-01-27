06e0834dabbbf105abb819eba8f804dd62198f55
Gustavo <luppi_classic@protonmail.com>
2022-01-27 15:02:14 -0300

    add solution #17

---
+++ decode_webpage.py
@@ -0,0 +1,19 @@
+# Decode A Web Page www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
+# Use the BeautifulSoup and requests Python packages to print out a list of all the article 
+# titles on the New York Times homepage.
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.nytimes.com/"
+req = requests.get(url)
+req_html = req.text
+soup = BeautifulSoup(req_html, 'html.parser')
+h3 = soup.find_all('h3') 
+
+def get_text():
+    text = ""
+    for i in h3:
+        text += i.text
+    print(text)
+
+get_text()
